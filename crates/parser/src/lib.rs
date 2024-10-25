1843e7bf51d8daadd9fce23e371cbbb2beee6c73
Ștefan-Iulian Alecu <165364995+vmaskmovps@users.noreply.github.com>
2024-10-25 17:29:39 +0300

    chore: reorganize project

---
+++ crates/parser/src/lib.rs
@@ -0,0 +1,7 @@
+// SPDX-FileCopyrightText: © 2024 Ștefan-Iulian Alecu <uneven-shiver@protonmail.com>
+//
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
