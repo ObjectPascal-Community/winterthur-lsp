7778ef2939178c6212ca0f8d3a68d8ad1962dca7
Ștefan-Iulian Alecu <165364995+vmaskmovps@users.noreply.github.com>
2024-10-25 04:22:34 +0300

    chore: add Rust skeleton project

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+// SPDX-FileCopyrightText: © 2024 Ștefan-Iulian Alecu <uneven-shiver@protonmail.com>
+//
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+fn main() {
+    println!("Hello, world!");
+}
